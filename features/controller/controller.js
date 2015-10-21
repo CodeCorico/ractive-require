5fb04a20aba65aa75fc98afca8e13826129c9f59
Xavier Boubert <xavier@boubert.fr>
2015-10-21 15:41:28 +0200

    refactor(require): replace "Component" to "component"
    "component" is a function

--- features/controller/controller.js
+++ features/controller/controller.js
@@ -11,10 +11,10 @@
     }
   };
 
-  function _callControllers(controllers, Component, data, el, config, i, callback) {
+  function _callControllers(controllers, component, data, el, config, i, callback) {
     if (i < controllers.length) {
-      controllers[i](Component, data, el, config, function() {
-        _callControllers(controllers, Component, data, el, config, ++i, callback);
+      controllers[i](component, data, el, config, function() {
+        _callControllers(controllers, component, data, el, config, ++i, callback);
       });
     }
     else if (callback) {
@@ -22,9 +22,9 @@
     }
   }
 
-  window.Ractive.fireController = function(name, Component, data, el, config, callback) {
+  window.Ractive.fireController = function(name, component, data, el, config, callback) {
     if (_controllers[name]) {
-      _callControllers(_controllers[name], Component, data, el, config, 0, callback);
+      _callControllers(_controllers[name], component, data, el, config, 0, callback);
     }
     else if (callback) {
       callback();
