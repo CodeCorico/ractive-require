fb5a01031de534b76dedb6601ed13fb256ab52c2
Xavier Boubert <xavier@boubert.fr>
2016-06-08 11:03:37 +0200

    chore(dist): build dist files

--- dist/ractive-require.js
+++ dist/ractive-require.js
@@ -1,4 +1,4 @@
-/*! Ractive-Require (0.6.0). (C) 2015 CodeCorico. MIT @license: en.wikipedia.org/wiki/MIT_License */
+/*! Ractive-Require (0.6.1). (C) 2015 CodeCorico. MIT @license: en.wikipedia.org/wiki/MIT_License */
 (function() {
   // Source: https://github.com/ractivejs/ractive-load/blob/master/src/utils/get.js
   // Author: Rich-Harris (https://github.com/Rich-Harris)
@@ -452,7 +452,7 @@
       ractive.on(event, function() {
         Array.prototype.unshift.call(arguments, name);
 
-        parent.apply(parent, arguments);
+        parent.fire.apply(parent, arguments);
       });
     };
 
