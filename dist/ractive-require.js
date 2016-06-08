e169d15f671ee23a6bdbd16774f51f9f163d0298
Xavier Boubert <xavier@boubert.fr>
2016-06-08 12:52:57 +0200

    chore(dist): build dist files

--- dist/ractive-require.js
+++ dist/ractive-require.js
@@ -1,4 +1,4 @@
-/*! Ractive-Require (0.6.1). (C) 2015 CodeCorico. MIT @license: en.wikipedia.org/wiki/MIT_License */
+/*! Ractive-Require (0.6.2). (C) 2015 CodeCorico. MIT @license: en.wikipedia.org/wiki/MIT_License */
 (function() {
   // Source: https://github.com/ractivejs/ractive-load/blob/master/src/utils/get.js
   // Author: Rich-Harris (https://github.com/Rich-Harris)
@@ -450,7 +450,7 @@
   function _fireBindedEvent(parent, ractive, databinding) {
     var fireevent = function(event, name) {
       ractive.on(event, function() {
-        Array.prototype.unshift.call(arguments, name);
+        Array.prototype.unshift.call(arguments, name, ractive);
 
         parent.fire.apply(parent, arguments);
       });
