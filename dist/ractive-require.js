b485ec86765eadaaafdc00b064776a3091a1fb7c
Xavier Boubert <xavier@boubert.fr>
2016-06-20 10:23:18 +0200

    chore(dist): build dist files

--- dist/ractive-require.js
+++ dist/ractive-require.js
@@ -1,4 +1,4 @@
-/*! Ractive-Require (0.6.3). (C) 2015 CodeCorico. MIT @license: en.wikipedia.org/wiki/MIT_License */
+/*! Ractive-Require (0.6.5). (C) 2016 CodeCorico. MIT @license: en.wikipedia.org/wiki/MIT_License */
 (function() {
   // Source: https://github.com/ractivejs/ractive-load/blob/master/src/utils/get.js
   // Author: Rich-Harris (https://github.com/Rich-Harris)
@@ -165,11 +165,19 @@
 
       if (attr.name.indexOf('data-on-') === 0) {
         name = attr.name.substr(8, attr.name.length - 8);
-        events[name] = attr.value;
+        name.split('-').forEach(function(eventName) {
+          if (eventName.trim()) {
+            events[eventName] = attr.value;
+          }
+        });
       }
       else if (attr.name.indexOf('data-listen-') === 0) {
         name = attr.name.substr(12, attr.name.length - 12);
-        listening[name] = attr.value;
+        name.split('-').forEach(function(eventName) {
+          if (eventName.trim()) {
+            listening[eventName] = attr.value;
+          }
+        });
       }
       else if (attr.name.indexOf('data-bind-') === 0) {
         name = attr.name.substr(10, attr.name.length - 10);
